4c8e2bfe3baa8a716b3cef01c1b2a92465b72c5a
Richard Mulero <richard.mulero@fcamara.com.br>
2019-02-22 15:02:29 -0300

    Implementando Marko

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -14,15 +14,20 @@ module.exports = (app) => {
     });
 
     app.get("/Livros", (req, res) => {
-        res.send(`
-        <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Listagem de Livros </h1>
-        </body> 
-        </html>`
+        res.marko(
+            require("../views/livros/lista/lista.marko"),
+            {
+                livros: [
+                    {
+                        id: 1,
+                        titulo: 'Fundamentos do Node'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Node Avançado'
+                    }
+                ]
+            }
         );
     });
 
