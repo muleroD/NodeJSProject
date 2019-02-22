1ed8a0152b9ca76e896e51aeea25cc626d67fb18
Richard Mulero <richard.mulero@fcamara.com.br>
2019-02-22 10:59:30 -0300

    Init nodeJs e incluindo Express 4.16.3

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-require('http');
+const http = require('http');
+const server = http.createServer(function (req, res) {
+
+    let html = "";
+
+    if (req.url == "/") {
+        html = `
+        <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Casa do Código </h1>
+        </body> 
+        </html>`;
+    } else if (req.url == "/livros") {
+        html = `
+        <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Listagem de Livros </h1>
+        </body> 
+        </html>`;
+    }
+
+    res.end(html)
+});
+
+server.listen(3000)
