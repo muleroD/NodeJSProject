b8b5bf265331b5137491b10c04aa884fe06113f6
Richard Mulero <richard.mulero@fcamara.com.br>
2019-02-22 11:53:10 -0300

    Criando arquivos de rotas

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
 const app = require("./src/config/configExpress")
 
-app.listen(3000, function () {
-    console.log("Server run in port 3000");
-    console.log("http://localhost:3000");
-});
-
-app.get("/", (req, res) => {
-    res.send(`
-        <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Casa do Código </h1>
-        </body> 
-        </html>`
-    );
+app.listen(4000, function () {
+    console.log("Server run in http://localhost:4000 ");
 });
